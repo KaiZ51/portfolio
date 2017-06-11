6c0289cb84f9a571453ce722a0620011e052a3f7
KaiZ51 <pedrosilva510@gmail.com>
2017-06-11 18:42:28 +0100

    Fixed Bootstrap not fully working (replaced with Bootstrap 3)

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
                 }
             },
             {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
-            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, loader: 'url-loader'}
         ]
     }
 };
