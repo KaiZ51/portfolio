9bfa4bca914b0faa09526ff3b6f6fedc151408c8
KaiZ51 <pedrosilva510@gmail.com>
2017-06-11 22:17:20 +0100

    Finished basic Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col} from 'react-bootstrap'
 import NavMenu from './NavMenu';
+import Intro from "./Intro";
 
 class App extends Component {
     render() {
         return (
             <Grid>
                 <Row>
-                    <Col xs={12}>
+                    <Col>
                         <NavMenu/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Intro/>
                     </Col>
                 </Row>
             </Grid>
---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Intro extends Component {
+    render() {
+        return (
+            <div style={{paddingLeft: '10%', paddingRight: '10%'}}>Hey there, welcome to my website! Here you can find some personal projects I've developed so far, with
+                more coming in the future.</div>
+        )
+    }
+}
+
+export default Intro;
