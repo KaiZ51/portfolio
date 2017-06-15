33dbd2cefbc0c36db9ccf879620a92e0cb6b7f1a
KaiZ51 <pedrosilva510@gmail.com>
2017-06-16 00:17:34 +0100

    Started implementing routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col, Image} from 'react-bootstrap';
+import {Router, Route} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import NavMenu from './NavMenu';
 import Intro from "./Intro";
+import Projects from "./Projects";
 
 class App extends Component {
     render() {
@@ -9,7 +12,13 @@ class App extends Component {
             <Grid>
                 <Row>
                     <Col>
-                        <NavMenu/>
+                        <Router history={createHistory()}>
+                            <div>
+                                <NavMenu/>
+                                <Route exact path="/" component={Intro}/>
+                                <Route path="/projects" component={Projects}/>
+                            </div>
+                        </Router>
                     </Col>
                 </Row>
                 <Row>
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import {Nav, Navbar, NavItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 class NavMenu extends Component {
     render() {
@@ -7,14 +9,16 @@ class NavMenu extends Component {
             <Navbar collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand style={{paddingTop: '8px', paddingBottom: '8px'}}>
-                        <a href="#">Pedro Silva</a>
+                        <Link to="/">Pedro Silva</Link>
                         <div style={{fontSize: 10}}>Software Developer</div>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavItem eventKey={1} href="#">Projects</NavItem>
+                        <LinkContainer to="/projects">
+                            <NavItem eventKey={1}>Projects</NavItem>
+                        </LinkContainer>
                         <NavItem eventKey={2} href="#">About Me</NavItem>
                         <NavItem eventKey={3} href="#">Contacts</NavItem>
                     </Nav>
