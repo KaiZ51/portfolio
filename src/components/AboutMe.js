9e399e99198833960da2071979774beed5c96af3
KaiZ51 <pedrosilva510@gmail.com>
2017-08-14 00:43:47 +0100

    Updated AboutMe and Intro components

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,22 +4,26 @@ import {Row, Col} from 'react-bootstrap';
 class AboutMe extends Component {
     render() {
         return (
-            <Row className="text-center" style={{paddingTop: '50px'}}>
-                <h3>Myself</h3>
-                <h5>My name is Pedro Silva, and I'm a guy from Portugal.</h5>
-                <h5>I've recently graduated from a Bachelor's degree in Computer Science.</h5>
+            <Row style={{paddingTop: '25px'}}>
+                <Col xs={12} className="about-padding text-center">
+                    <h3>Myself</h3>
+                    <p>My name is Pedro Silva, and I'm a software developer from Portugal.</p>
+                    <p>I've recently graduated from a Bachelor's degree in Computer Science.</p>
 
-                <h3 style={{paddingTop: '3%'}}>Development experience</h3>
-                <h5>So far, I've worked mainly with Android apps and Web Development. On the web development side, I
-                    have experience with HTML, JavaScript, CSS, PHP, and most recently, ReactJS.</h5>
-                <h5>Aside from those, I've also got experience with MySQL, PostgreSQL, C#, Java, Firebase (database and
-                    otherwise), and Unity Game Engine.</h5>
+                    <h3 style={{paddingTop: '30px'}}>Development experience</h3>
+                    <p>So far, I've worked mainly with Android apps and Web Development.</p>
+                    <p>On the web development side, I have experience with HTML, JavaScript, CSS, Bootstrap, PHP, and
+                        most recently, ReactJS.</p>
+                    <p>Aside from those, I've also got experience with MySQL, PostgreSQL, C#, Java, Firebase (database
+                        and otherwise), and the Unity Game Engine.</p>
 
-                <h3 style={{paddingTop: '3%'}}>Interests</h3>
-                <h5>As for my interests, ever since I was very young, I've always had an interest in technology in some
-                    form, whether its computers, phones, or even games.</h5>
-                <h5>That still holds true to this day, and as such, I'm constantly looking to expand my knowledge.</h5>
-                <h5>I'm also very passionate about gaming and music, and love traveling whenever possible.</h5>
+                    <h3 style={{paddingTop: '30px'}}>Interests</h3>
+                    <p>As for my interests, ever since I was very young, I've always had an interest in technology in
+                        some form, whether its computers, phones, or even games.</p>
+                    <p>That still holds true to this day, and as such, I'm constantly looking to expand my
+                        knowledge.</p>
+                    <p>I'm also very passionate about gaming and music, and love traveling whenever possible.</p>
+                </Col>
             </Row>
         )
     }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,15 +6,15 @@ class Intro extends Component {
     render() {
         return (
             <Row style={{paddingTop: '50px'}}>
-                <Col>
+                <Col xs={12}>
                     <Image className="center-block" src="http://via.placeholder.com/100x100" circle/>
                 </Col>
-                <Col className="text-center">
+                <Col xs={12} className="text-center" style={{paddingTop: '15px'}}>
                     <h3>Hey there, welcome to my website!</h3>
-                    <h5>Here you can find some personal projects I've developed so far, with
-                        more coming in the future.</h5>
-                    <h5>Currently I'm available for hiring, so feel free to <Link to="/contacts">contact
-                        me</Link> after checking out my <Link to="/projects">projects</Link>.</h5>
+                    <p>Here you can find some personal projects I've developed so far, with
+                        more coming in the future.</p>
+                    <p>Currently I'm available for hiring, so feel free to <Link to="/contacts">contact
+                        me</Link> after checking out my <Link to="/projects">projects</Link>.</p>
                 </Col>
             </Row>
         )
