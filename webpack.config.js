118dc498f4c3aac1f507a4084669f0d246b5fbf6
KaiZ51 <pedrosilva510@gmail.com>
2017-08-21 00:29:49 +0100

    - Fixed publicPath.
    - Removed gh-pages package.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
-        publicPath: path.resolve(__dirname, './dist') + '/'
+        publicPath: './dist/'
     },
     module: {
         loaders: [
