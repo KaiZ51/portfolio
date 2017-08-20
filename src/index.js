1fb774734b07a619a76056c8bbeae04fc76740c5
KaiZ51 <pedrosilva510@gmail.com>
2017-08-20 23:19:32 +0100

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap/css/bootstrap.min.css';
-import './index.css';
-import App from './components/App';
-
-ReactDOM.render(
-    <App />, document.getElementById('root')
-);
