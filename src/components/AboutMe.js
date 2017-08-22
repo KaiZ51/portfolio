0e5c63b40628b7292340547c18e6fe73941605a2
Pedro Silva <pedrosilva510@gmail.com>
2017-08-22 12:22:18 +0100

    Restructured text in AboutMe component.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ class AboutMe extends Component {
                 <Col xs={12} className="about-padding text-center">
                     <h2>Myself</h2>
                     <p>My name is <b>Pedro Silva</b>, and I'm a <b>software developer</b> from Portugal.</p>
-                    <p>I've recently graduated from a Bachelor's degree in <b>Computer Science</b>.</p>
+                    <p>I've recently graduated with a Bachelor's degree in <b>Computer Science</b>.</p>
 
                     <h2 style={{paddingTop: '40px'}}>Development experience</h2>
                     <p>So far, I've worked mainly with Android app and web development.</p>
@@ -18,8 +18,8 @@ class AboutMe extends Component {
                         <b> Firebase</b> (database and otherwise), and the <b>Unity</b> game engine.</p>
 
                     <h2 style={{paddingTop: '40px'}}>Interests</h2>
-                    <p>As for my interests, ever since I was very young, I've always had an interest in technology in
-                        some form, whether its computers, phones, or even games.</p>
+                    <p>Ever since I was very young, I've always been fond of technology in some form, whether its
+                        computers, phones, or even games.</p>
                     <p>That still holds true to this day, and as such, I'm constantly looking to expand my
                         knowledge and tinker with new stuff.</p>
                     <p>I'm also very passionate about gaming and music, and love traveling whenever possible.</p>
