917c12b958afb993ccef599992a35a912ac87172
KaiZ51 <pedrosilva510@gmail.com>
2018-03-18 18:42:27 +0000

    Changed text on intro page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,9 +15,8 @@ export default class Intro extends Component {
                     <p>I'm a <b>software developer</b>, mainly focused on <b>web/mobile</b> development.</p>
                     <p>Here you can find some personal projects I've developed so far, with
                         more coming in the future.</p>
-                    <p>Currently I'm available for hiring, so feel free to <Link to="/contacts">contact
-                        me</Link> after checking out my <Link to="/projects">projects</Link> and <Link
-                        to="/aboutme"> development experience</Link>.</p>
+                    <p>Feel free to <Link to="/contacts">contact me</Link> after checking out my <Link
+                        to="/projects">projects</Link> and <Link to="/aboutme"> development experience</Link>.</p>
                 </Col>
             </Row>
         );
