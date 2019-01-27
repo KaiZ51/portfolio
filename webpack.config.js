62f6418f3d77d8a89148d92d9b37169839900148
Pedro <pedrosilva510@gmail.com>
2019-01-27 00:14:48 +0000

    Updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ["react", "env"]
+                    presets: ["@babel/preset-react", "@babel/preset-env"]
                 }
             },
             {
